f3f59dc4725b2288f49c424e9110e5900d79f72c
beltrewilton <beltre.wilton@gmail.com>
2025-10-26 18:24:09 -0400

    version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule DgiiGw.MixProject do
     [
       app: :dgii_gw,
       version: "0.1.0",
-      elixir: "~> 1.18",
+      elixir: "~> 1.17.2",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
